97d835b8b3e71ebfa32e11ad41340b20d1cd90c1
unknown <xemiruiz@gmail.com>
2023-04-19 09:31:29 +0200

    actualizacion de comentarios dentro del metodo y modificacion del correo de developer

--- src/main/java/com/nttdata/mvn/App.java
+++ src/main/java/com/nttdata/mvn/App.java
@@ -3,12 +3,17 @@ package com.nttdata.mvn;
 /**
  * Metodo main donde se llama al metodo Password.
  *
+ *@Param args
  */
 public class App {
 	public static void main(String[] args) {
 
+		
+		//Inicializamos y creamos un objeto Password
 		Password p1 = new Password();
-
+		
+		
+		//Ponemos por pantalla el metodo creationPass
 		System.out.println(p1.creationPass(10));
 	}
 }
